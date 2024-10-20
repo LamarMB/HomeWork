55dc36107b543c01f6c49fde93936074cc2b78c7
Urban <soshin1992@bk.ru>
2024-10-20 20:15:51 +0300

    15.10.2024

---
+++ list.py
@@ -0,0 +1,10 @@
+food = ['aple', 'cocount', 'banana']
+print(food[0])
+food[0]= 'kivi'
+print(food)
+food.append(True)
+print(food)
+food.extend(['salam', 2])
+print(food)
+food.remove('banana')
+print(food[0: 2])
