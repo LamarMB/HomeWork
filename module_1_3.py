6f527bf9ebeaa1f8fdb6f2351c27fc787eda9721
Urban <soshin1992@bk.ru>
2024-10-20 09:48:25 +0300

    15.10.2024

--- module_1_3.py
+++ module_1_3.py
@@ -1,8 +1,14 @@
-name='Мурад'
-print(name)
-age= 23
-print(age)
-age=24
-print(age)
-is_student =True
-print(is_student)
+houm_work = 12
+hours_spent = 1.5
+course = 'Python'
+time_for_one_task = 6
+
+print ('Курс:' , (course), (",") , 'Всего задач:', (houm_work), (","), 'Затрачено часов:',(hours_spent), (",") , 'Среднее время выполнения:', (time_for_one_task),'мин')
+
+
+first_name = 'Мурад'
+apple_count = 8
+second_name = 'Алексею'
+orange_count = 9
+
+print(first_name, 'дал', second_name , apple_count , 'яблок и' , orange_count , 'апельсинов')
